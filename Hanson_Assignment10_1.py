efae4a022c3431ec0d6850bfeea4e16a4459d7c8
chanso-n <84053697+chanso-n@users.noreply.github.com>
2021-05-23 17:23:54 -0400

    Add files via upload

---
+++ Hanson_Assignment10_1.py
@@ -0,0 +1,22 @@
+import os
+import csv
+
+working_directory = input('What directory would you like to use? ')
+
+os.chdir(working_directory)
+
+filename = input('What would you like to name your file? ')
+
+name = input('What is your name? ') + ', '
+address = input('What is your address? ') + ', '
+phone_number = input('What is your phone number? ')
+
+
+with open(filename, 'a') as f:
+    f.write(name)
+    f.write(address)
+    f.write(phone_number)
+
+
+with open(filename, 'r') as reader:
+    print(reader.read())
